4b712e02ee8f755c9b299f87109c5e1c111915df
SSChen09 <sansanchen09@gmail.com>
2024-12-15 14:05:48 +0800

    Update 1_Gemini_Pro.py

--- 1_Gemini_Pro.py
+++ 1_Gemini_Pro.py
@@ -20,7 +20,7 @@ st.caption("a chatbot, powered by google gemini pro.")
 
 
 if "app_key" not in st.session_state:
-    app_key = st.text_input("Your Gemini App Key", type='text')
+    app_key = st.text_input("Your Gemini App Key", type='default')
     if app_key:
         st.session_state.app_key = app_key
 
